592088c8ed8cae7ff6336f4ff49a65cd03d23ea7
chey3002 <carlos-avb@hotmail.com>
2020-04-05 01:59:04 -0500

    Page
    Falta por terminar

---
+++ index.php
@@ -0,0 +1,178 @@
+
+<!-- Price box minimal--><!DOCTYPE html>
+<html class="wide wow-animation" lang="en">
+  <head>
+    <title>Home</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,900">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/fonts.css">
+    <link rel="stylesheet" href="css/style.css">
+    <style>.ie-panel{display: none;background: #212121;padding: 10px 0;box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3);clear: both;text-align:center;position: relative;z-index: 1;} html.ie-10 .ie-panel, html.lt-ie-10 .ie-panel {display: block;}</style>
+  </head>
+  <body>
+    
+      <!-- Preview section-->
+      <section class="section context-dark bg-image bg-mask bg-mask-2 section-fullheight section-90vh" style="background-image: url(images/building-sign-light-neon.jpg)">
+        <div class="section-fullheight-inner section-md">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-8 col-xl-6 text-center text-md-left">
+                <div class="jumbotron-custom wow fadeInRight">
+                  <div class="text-1">Carlos Valladarez</div>
+                  <div class="text-2"></div>
+                  <div class="title">Developer</div>
+                  <div class="text-2" >Estudiante de ingeniería de sistemas con solidas bases de programación, desarrollo web, arquitectura de software, Bases de datos SQL, y no SQL.</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- EVERY PROJECT-->
+      <section class="section section-xl-custom image-section section-lined">
+        <div class="container container-lined">
+          <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-4"></div>
+            <div class="col-md-4"></div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-7 col-lg-6">
+              <h3>EVERY PROJECT</h3>
+              <div class="big big-custom">IS A CHALLENGE TO DO BETTER</div>
+              <p>We are a team of  talented designers and developers who are ready to guide our clients through the flawless and timely execution of any web design project. Since day one, we have been delivering creative and unique websites to our clients worldwide.</p>
+            </div>
+          </div>
+        </div>
+        <div class="image-container">
+          <div class="mock-up-1">
+            <div class="object object-1"><a href="#"><img class="wow fadeInRight" src="images/image-custom-1-395x243.jpg" alt="" width="395" height="243"/></a>
+            </div>
+            <div class="object object-2"><img class="wow fadeInRight" src="images/image-custom-2-384x189.jpg" alt="" width="384" height="189" data-wow-delay="0.2s"/>
+            </div>
+            <div class="object object-3"><img class="wow fadeInRight" src="images/image-custom-3-395x243.jpg" alt="" width="395" height="243" data-wow-delay="0.4s"/>
+            </div>
+            <div class="object object-4"><img class="wow fadeInRight" src="images/image-custom-4-209x184.jpg" alt="" width="209" height="184" data-wow-delay="0.6s"/>
+            </div>
+            <div class="object object-5"><img class="wow fadeInRight" src="images/image-custom-5-395x243.jpg" alt="" width="395" height="243" data-wow-delay="0.2s"/>
+            </div>
+            <div class="object object-6"><img class="wow fadeInRight" src="images/image-custom-6-275x184.jpg" alt="" width="275" height="184" data-wow-delay="0.4s"/>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- Latest Works-->
+      <section class="section section-xs section-xs-3 bg-default section-lined">
+        <div class="container container-lined">
+          <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-4"></div>
+            <div class="col-md-4"></div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row row-30">
+            <div class="col-sm-6">
+              <h3><a href="/site/CV/HojaDeVida.pdf" >Hoja de Vida</a></h3>
+            </div>
+            <div class="col-sm-6 text-sm-right"><a class="button button-default"  target="_blank" href="https://github.com/chey3002?tab=repositories">View all projects</a></div>
+          </div>
+        </div>
+      </section>
+      <!-- Latest Works-->
+      <section class="section bg-gray-700 context-dark section-relative">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-5 text-block-1 wow fadeInLeft">
+              <div class="section-sm section-sm-3">
+                <h5>Aptitudes</h5>
+                <p>Adaptabilidad
+                </br>
+                Capacidad de resolver problemas
+                </br>
+                Seriedad
+                </br>
+                Seguridad</p>
+                <ul class="list-inline list-inline-sm-1">
+                  <li>
+                    <p>Encuentrame en:</p>
+                  </li>
+                  <li><a class="icon icon-sm link-default mdi mdi-facebook" href="https://www.facebook.com/CarlosAVB"></a></li>
+                  <li><a class="icon icon-sm link-default mdi mdi-linkedin" href="https://www.linkedin.com/in/carlos-avb/"></a></li>
+                  <li><a class="icon icon-sm link-default mdi mdi-twitter" href="https://twitter.com/Sr_Chey"></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Swiper-->
+        <div class="swiper-section">
+          <div class="swiper-container swiper-slider swiper-slider-1" data-next=".custom-swiper-button-next" data-prev=".custom-swiper-button-prev" data-loop="true">
+            <div class="swiper-wrapper">
+              <div class="swiper-slide" >
+              <h3>Experiencia laboral</h3>
+              </br>
+              <h10>Toque de Queda Exposición Artística</h10>
+              <p>Enero 2020 - Febrero 2020</p>
+              <p>Desarrollador web:  Desarrollo de la página web para "Toque de Queda Exposición Artística"</p>
+              <h10>Superintendencia de Ordenamiento Territorial, Uso y Gestión del Suelo</h10>
+              <p>Agosto 2019 - Septiembre 2019</p>
+              <p>Prácticas de ingeniería: Se desarrollo una aplicación web- con nodejs, express, y ejs- que permita gestionar usuarios y archivos para los GADs, en una base de datos noSQL</p>
+              
+              </div>
+              <div class="swiper-slide" >
+              <h3>Educación</h3>
+              </br>
+              <h5>Universidad de Cuenca</h5>
+              <p>Ingeniería de sistemas</p>
+              </br>
+              <h5>Colegio Tecnico Salesiano</h5>
+              <p>BACHILLER TECNICO INDUSTRIAL ESPECIALIDAD MECATRONICA-2013</p>
+              </div>
+              <div class="swiper-slide" >
+              <h3>Experiencia de voluntariado</h3>
+              </br>
+              <h5>Microsoft Student UCuenca</h5>
+              <p>PROMOVIENDO EL USO DE LAS TIC EN ESTUDIANTES DE PARROQUIAS RURALES DEL CANTÓN CUENCA
+              <br>
+              Instructor
+              <br>
+              Septiembre 2018-Junio 2019</p>
+              </div>
+            </div>
+            <div class="swiper-pagination"></div>
+            <!-- Swiper Navigation-->
+          </div>
+          <div class="custom-swiper-button-wrap">
+            <div class="custom-swiper-button-next mdi mdi-arrow-right"></div>
+            <div class="custom-swiper-button-prev mdi mdi-arrow-left"></div>
+          </div>
+        </div>
+      </section>
+      
+      <footer class="section footer-classic context-dark text-center">
+        <div class="container">
+          <div class="row row-15 justify-content-lg-between">
+            <div class="col-lg-6 col-xl-6 text-lg-left">
+            </div>
+            <div class="col-lg-6 col-xl-6">
+              <ul class="list-inline list-inline-lg text-uppercase">
+                <li>by: <a href="https://www.linkedin.com/in/carlos-avb/">Chey3002</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+    <div class="snackbars" id="form-output-global"></div>
+    <script src="js/core.min.js"></script>
+    <script src="js/script.js"></script>
+    <!-- coded by Drel-->
+  </body>
+</html>
